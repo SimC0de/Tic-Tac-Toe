bdff52e2fceb82eb4690a9022fa350da6152f860
R3d-Reaper <100780725+R3d-Reaper@users.noreply.github.com>
2024-02-26 19:51:08 +0800

    Building easy difficulty for computer opponent

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import random
 
 print("##### #####  ###      #####  ###   ###      ##### ##### #####") 
 print("  #     #   #   #       #   #   # #   #       #   #   # #    ")
@@ -7,11 +9,6 @@ print("  #     #   #   #       #   #   # #   #       #   #   # #    ")
 print("  #   #####  ###        #   #   #  ###        #   ##### #####")  
 print()
 print("=============================================================")
-print()
-
-player_vs = input("Player vs...\n1. Player\n2. Computer\n")
-
-ttt_area_num = ['', '', '', '', '', '', '', '', '']
 
 # #1 = index #14, #2 = index #18, #3 = index #22
 # #4 = index #62, #5 = index #66, #6 = index #70
@@ -30,3 +27,29 @@ ttt_area = """
    |   |    """
 
 print(ttt_area)
+print()
+
+choose_opponent = input("Choose your opponent\n1. Player\n2. Computer\nTo close the program, press 3 or exit\n")
+
+while choose_opponent:
+    if choose_opponent == "1":
+        print("1 works")
+        choose_opponent = False
+    elif choose_opponent == "2":
+        choose_computer = input("Choose computer difficulty\n1. Easy\n2. Medium(Not Available)\n3. Hard\n")
+        while choose_computer:
+            if choose_computer == "1":
+                game_done = False
+                while game_done == False:
+                    print("=============================================================")
+                    print()
+                    print(ttt_area)
+                    player1_choice = input("What area would you like to mark?\n")
+                    computer_choice = random.randint(1, 9)
+                    print(f"computer marked area {computer_choice}")
+        choose_opponent = False
+    elif choose_opponent == "3":
+        print("3 words")
+        choose_opponent = False
+    else:
+        choose_opponent = input("Not a valid choice\nInput \"1\" to choose another player as an opponent...\nInput \"2\" to choose a computer as an opponent...\nInput \"3\" to to exit the program...\n")
