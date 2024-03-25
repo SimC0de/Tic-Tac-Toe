966e8dbcb06b81744df88e5620444d5acc456521
SimC0de <yor1simone@outlook.com>
2024-03-25 13:59:23 +0800

    Able to end the game when a winning condition is given

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -33,37 +33,48 @@ choose_opponent = input("Choose your opponent\n1. Player\n2. Computer\nTo close 
 
 while choose_opponent:
     if choose_opponent == "1":
-        print("1 works")
-        choose_opponent = False
+        choose_opponent = input('This is still in devlopment, please choose computer for your opponent\n1. Player\n2. Computer\nTo close the program, press 3 or exit\n')
     elif choose_opponent == "2":
         choose_computer = input("Choose computer difficulty\n1. Easy\n2. Medium(Not Available)\n3. Hard\n")
         while choose_computer:
             # Easy Computer
             if choose_computer == "1":
-                game_ttt_area = ttt_area
+                # gta, short for game tic-tac-toe area
+                gta = ttt_area
                 while True:
                     print("=============================================================")
                     print()
-                    print(game_ttt_area)
+                    print(gta)
                     # Player 1 Turn
                     player1_choice = input("What area would you like to mark?\n")
-                    while player1_choice.isnumeric() == False and player1_choice == " ":
+                    while player1_choice.isnumeric() == False or player1_choice == " " or player1_choice not in gta:
                         print("No")
-                        player1_choice = input("Marked area or invalid input")
-                    add_mark = game_ttt_area.replace(player1_choice, "O")
-                    game_ttt_area = add_mark
-                    player1_choice = False
-                    if (game_ttt_area[13] == "O" and game_ttt_area[17] == "O" and game_ttt_area[21] == "O"):
+                        player1_choice = input("Marked area or invalid input\n")
+                    add_mark = gta.replace(player1_choice, "O")
+                    gta = add_mark
+                    if gta[13] == 'O' and gta[17] == 'O' and gta[21] == 'O':
+                        print(gta)
                         print("Player Won")
+                        choose_computer = 'end'
                         break
                     # Computer Turn
                     computer_choice = str(random.randint(1, 9))
-                    add_mark = game_ttt_area.replace(computer_choice, "X")
-                    game_ttt_area = add_mark
+                    while computer_choice not in gta:
+                        computer_choice = str(random.randint(1, 9))
+                    add_mark = gta.replace(computer_choice, "X")
+                    gta = add_mark
                     print(f"computer marked area {computer_choice}")
+            elif choose_computer == "2":
+                choose_computer = input("\'Easy\' is the only available mode at the moment\n")
+            elif choose_computer == "3":
+                choose_computer = input("\'Easy\' is the only available mode at the moment\n")
+            elif choose_computer == 'end':
+                print("Game over!")
+                break
         choose_opponent = False
     elif choose_opponent == "3":
         print("3 words")
         choose_opponent = False
+        print("Game over!")
     else:
         choose_opponent = input("Not a valid choice\nInput \"1\" to choose another player as an opponent...\nInput \"2\" to choose a computer as an opponent...\nInput \"3\" to to exit the program...\n")
