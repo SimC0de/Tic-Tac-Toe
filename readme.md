f4fee8388ade6956776549722589eda3bd8d3454
SimC0de <yor1simone@outlook.com>
2024-03-26 17:01:14 +0800

    Added all the winning condition for player and computer in easy mode, added a condition to reset the area if all are marked down

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Tic-Tac-Toe Terminal Game
+
+---
+
+## Description
+Hello and welcome to my project, a Tic-Tac-Toe terminal game made using only python. I started this project to finish the lessons on my Computer Science course on CodeCademy and also to familiarize myself with what I have learned over the months. The code's readability is not good right now at the moment but I am satisfied with what I have made so far.
+
+---
+
+## How to play
+1. Find an application to run the file
+    - if you are going to use the CMD, change the directory where the file is located and type 'tic-tac-toe.py'
+    - if you are going to use an IDE like Visual Studio Code, just run the file and use the built in terminal of that IDE
+2. Choose an opponent
+    - as of this moment, 'Computer' opponent is only available
+3. Choose a difficulty
+    - as of this moment, 'Easy' mode is only available
+4. Play and enjoy the game
+
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -29,12 +29,15 @@ ttt_area = """   |   |
 print(ttt_area)
 print()
 
+# Choose opponent
 choose_opponent = input("Choose your opponent\n1. Player\n2. Computer\nTo close the program, press 3 or exit\n")
-
 while choose_opponent:
+    # Choose player as opponent
     if choose_opponent == "1":
         choose_opponent = input('This is still in devlopment, please choose computer for your opponent\n1. Player\n2. Computer\nTo close the program, press 3 or exit\n')
+    # Choose computer as opponent
     elif choose_opponent == "2":
+        #Choose computer difficulty
         choose_computer = input("Choose computer difficulty\n1. Easy\n2. Medium(Not Available)\n3. Hard\n")
         while choose_computer:
             # Easy Computer
@@ -45,6 +48,18 @@ while choose_opponent:
                     print("=============================================================")
                     print()
                     print(gta)
+                    # Check if areas are all marked before player's turn
+                    areas = [gta[13], gta[17], gta[21], gta[61], gta[65], gta[69], gta[109], gta[113], gta[117]]
+                    areas_full = False
+                    for area in areas:
+                        if area == 'X' or area == 'O':
+                            areas_full = True
+                        else:
+                            areas_full = False
+                            break
+                    if areas_full:
+                        gta = ttt_area
+                        print("Removing marks, Player 1 will have the first Turn!")
                     # Player 1 Turn
                     player1_choice = input("What area would you like to mark?\n")
                     while player1_choice.isnumeric() == False or player1_choice == " " or player1_choice not in gta:
@@ -52,11 +67,32 @@ while choose_opponent:
                         player1_choice = input("Marked area or invalid input\n")
                     add_mark = gta.replace(player1_choice, "O")
                     gta = add_mark
-                    if gta[13] == 'O' and gta[17] == 'O' and gta[21] == 'O':
-                        print(gta)
+                    print(gta)
+                    print()
+                    print("=============================================================")
+                    print()
+                    # Player 1 Winning Conditions
+                    if (gta[13] == 'O' and gta[17] == 'O' and gta[21] == 'O') or (gta[61] == 'O' and gta[65] == 'O' and gta[69] == 'O') or (gta[109] == 'O' and gta[113] == 'O' and gta[117] == 'O') or (gta[13] == 'O' and gta[61] == 'O' and gta[109] == 'O') or(gta[17] == 'O' and gta[65] == 'O' and gta[113] == 'O') or (gta[21] == 'O' and gta[69] == 'O' and gta[117] == 'O') or (gta[13] == 'O' and gta[65] == 'O' and gta[117] == 'O') or (gta[21] == 'O' and gta[65] == 'O' and gta[109] == 'O'):
                         print("Player Won")
-                        choose_computer = 'end'
+                        # Choice to continue or not
+                        choose_computer = input('Would you like to continue?\nType \'yes\' to continue, \'no\' to end the game\n').lower()
+                        if choose_computer == 'yes':
+                            choose_computer = '1'
+                        else:
+                            choose_computer = 'end'
                         break
+                    # Check if areas are all marked before computer's turn
+                    areas = [gta[13], gta[17], gta[21], gta[61], gta[65], gta[69], gta[109], gta[113], gta[117]]
+                    areas_full = False
+                    for area in areas:
+                        if area == 'X' or area == 'O':
+                            areas_full = True
+                        else:
+                            areas_full = False
+                            break
+                    if areas_full:
+                        gta = ttt_area
+                        print("Removing marks, Player 1 will have the first Turn!")
                     # Computer Turn
                     computer_choice = str(random.randint(1, 9))
                     while computer_choice not in gta:
@@ -64,14 +100,30 @@ while choose_opponent:
                     add_mark = gta.replace(computer_choice, "X")
                     gta = add_mark
                     print(f"computer marked area {computer_choice}")
+                    print()
+                    # Computer winning conditions
+                    if (gta[13] == 'X' and gta[17] == 'X' and gta[21] == 'X') or (gta[61] == 'X' and gta[65] == 'X' and gta[69] == 'X') or (gta[109] == 'X' and gta[113] == 'X' and gta[117] == 'X') or (gta[13] == 'X' and gta[61] == 'X' and gta[109] == 'X') or(gta[17] == 'X' and gta[65] == 'X' and gta[113] == 'X') or (gta[21] == 'X' and gta[69] == 'X' and gta[117] == 'X') or (gta[13] == 'X' and gta[65] == 'X' and gta[117] == 'X') or (gta[21] == 'X' and gta[65] == 'X' and gta[109] == 'X'):
+                        print(gta)
+                        print("Computer Won")
+                        # Choose to continue or not
+                        choose_computer = input('Would you like to continue?\nType \'yes\' to continue, or any value to end the game\n').lower()
+                        if choose_computer == 'yes':
+                            choose_computer = '1'
+                        else:
+                            choose_computer = 'end'
+                        break
+            # Medium Computer
             elif choose_computer == "2":
                 choose_computer = input("\'Easy\' is the only available mode at the moment\n")
+            # Hard Computer
             elif choose_computer == "3":
                 choose_computer = input("\'Easy\' is the only available mode at the moment\n")
+            # Condition to end the terminal game
             elif choose_computer == 'end':
                 print("Game over!")
                 break
         choose_opponent = False
+    # Exit the terminal game
     elif choose_opponent == "3":
         print("3 words")
         choose_opponent = False
