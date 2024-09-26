7e79724a6d6ad9bf83d2b032ca0a60fb20cf3feb
SimC0de <yor1simone@outlook.com>
2024-09-26 22:46:25 +0800

    Changed the  Ascii Arts (Unfinished/Undecided)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,4 +3,19 @@ from ascii_arts import *
                  
 print(header)      
 print(board)           
-                                               
+
+# Choose opponent through user inpute
+opponent_input = input("Choose your opponent:\n1. Player (PvP)\n2. Computer (PvC)\n")       
+
+# Check if user is still choosing an opponent
+choose_opponent = True
+while(choose_opponent):
+    if(opponent_input == "1"):
+        print("Fighting Player")
+        choose_opponent = False
+    elif(opponent_input == "2"):
+        print("Fighting Computer")
+        choose_opponent = False
+    else:
+        opponent_input = input("Please choose an opponent using the numbers that corresponds with them\n\nChoose your opponent:\n1. Player (PvP)\n2. Computer (PvC)\n")
+                           
