5fca4173a60779a0c9c6b14a3f9b7d9c25a1bce5
SimC0de <yor1simone@outlook.com>
2024-09-27 12:23:32 +0800

    User input messages
    A seperate file for the input messages to improve readability in the tic_tac_toe.py file

---
+++ messages.py
@@ -0,0 +1,10 @@
+message = """Choose your opponent:
+1. Player
+2. Computer"""
+
+warning_message = f"""
+Please choose a number that corresponds with your opponent (e.g. \"1\" for Player)
+{message}
+        """
+
+print(warning_message)
