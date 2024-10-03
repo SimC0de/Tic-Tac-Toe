f731edf1c09d1ea02cf71e3dd0ba65f408bc9fdc
SimC0de <yor1simone@outlook.com>
2024-10-03 13:50:22 +0800

    Fixed a minor error in while loop player's turn and added a warning message if input is already taken or not valid

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -28,43 +28,37 @@ if (opponent == "Player"):
     #Temporary block references
     tbr = block_references
     while (game_is_over != True):
-        player1_turn = input(player1_turn_message)
-        player1_turn_validation = player1_turn in tbr and player1_turn != "".strip()
-        while (player1_turn_validation):
-            if player1_turn_validation:
-                tbr = list(map(lambda x: x.replace(player1_turn, 'O'), tbr))
-                print(tbr)
-                tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
-                              [tbr[3], tbr[4], tbr[5]],
-                              [tbr[6], tbr[7], tbr[8]],
-                              [tbr[0], tbr[3], tbr[6]],
-                              [tbr[1], tbr[4], tbr[7]],
-                              [tbr[2], tbr[5], tbr[8]],
-                              [tbr[0], tbr[4], tbr[8]],
-                              [tbr[2], tbr[4], tbr[6]],]
-                print(tbr_winning_conditions)
-                break
+        player_1_turn = input(player_1_turn_message)
+        while bool(player_1_turn in tbr and player_1_turn != "".strip()) == False:
+            player_1_turn = input(turn_warning_message)
+        tbr = list(map(lambda x: x.replace(player_1_turn, 'O'), tbr))
+        tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
+                      [tbr[3], tbr[4], tbr[5]],
+                      [tbr[6], tbr[7], tbr[8]],
+                      [tbr[0], tbr[3], tbr[6]],
+                      [tbr[1], tbr[4], tbr[7]],
+                      [tbr[2], tbr[5], tbr[8]],
+                      [tbr[0], tbr[4], tbr[8]],
+                      [tbr[2], tbr[4], tbr[6]],]
+        print(tbr_winning_conditions)
         for win_condition in tbr_winning_conditions:
             if (len(set(win_condition)) == 1):
                 print("Player 1 Wins!")
                 game_is_over = True
                 break
-        player2_turn = input(player2_turn_message)
-        player2_turn_validation = player2_turn in tbr and player2_turn != "".strip()
-        while (player2_turn_validation):
-            if player2_turn_validation:
-                tbr = list(map(lambda x: x.replace(player2_turn, 'X'), tbr))
-                print(tbr)
-                tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
-                              [tbr[3], tbr[4], tbr[5]],
-                              [tbr[6], tbr[7], tbr[8]],
-                              [tbr[0], tbr[3], tbr[6]],
-                              [tbr[1], tbr[4], tbr[7]],
-                              [tbr[2], tbr[5], tbr[8]],
-                              [tbr[0], tbr[4], tbr[8]],
-                              [tbr[2], tbr[4], tbr[6]],]
-                print(tbr_winning_conditions)
-                break
+        player_2_turn = input(player_2_turn_message)
+        while bool(player_2_turn in tbr and player_2_turn != "".strip()) == False:
+            player_2_turn = input(turn_warning_message)
+        tbr = list(map(lambda x: x.replace(player_2_turn, 'X'), tbr))
+        tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
+                      [tbr[3], tbr[4], tbr[5]],
+                      [tbr[6], tbr[7], tbr[8]],
+                      [tbr[0], tbr[3], tbr[6]],
+                      [tbr[1], tbr[4], tbr[7]],
+                      [tbr[2], tbr[5], tbr[8]],
+                      [tbr[0], tbr[4], tbr[8]],
+                      [tbr[2], tbr[4], tbr[6]],]
+        print(tbr_winning_conditions)
         for win_condition in tbr_winning_conditions:
             if (len(set(win_condition)) == 1):
                 print("Player 2 Wins!")
