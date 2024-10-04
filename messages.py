0deb917a186d012d5f940d6cfb8867d29d45257b
SimC0de <yor1simone@outlook.com>
2024-10-04 10:20:50 +0800

    Slightly changed tabs and spaces on warning_message

--- messages.py
+++ messages.py
@@ -3,8 +3,8 @@ message = """Choose your opponent:
 (2) Computer
 """
 
-warning_message = f"""
-Please choose a number that corresponds with your opponent (e.g. \"1\" for Player)
+warning_message = f"""Please choose a number that corresponds with your opponent (e.g. \"1\" for Player)
+
 {message}
 """
 
