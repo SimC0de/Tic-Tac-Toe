217a454fe2cd13edc3f601f44a6d8f3ff51892ff
SimC0de <yor1simone@outlook.com>
2024-10-04 10:19:56 +0800

    Added dynamic numbered board
    Added lsb using list_number_board\nAdded game_board using lsb\nAdded a for loop that replaces the block by index in lsb depending on player's input and turn then prints it afterward

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,7 +5,7 @@ import time
 
 os.system('cls')
 print(header)      
-print(empty_board)           
+print(title_board)           
 
 input("Press enter to start:\n")
 os.system('cls')
@@ -15,27 +15,32 @@ choose_opponent_input = input(message)
 
 # Checks the chosen opponent
 opponent = ""
-while (choose_opponent_input):
-    if (choose_opponent_input == "1"):
-        print("Fighting Player")
-        opponent = "Player"
-        break
-    elif (choose_opponent_input == "2"):
-        print("Fighting Computer")
-        opponent = "Computer"
-        break
-    else:
-        choose_opponent_input = input(warning_message)
+while (bool(choose_opponent_input) != True):
+    os.system('cls')
+    choose_opponent_input = input(warning_message)
+
+if (choose_opponent_input == "1"):
+    print("Fighting Player")
+    opponent = "Player"
+elif (choose_opponent_input == "2"):
+    print("Fighting Computer")
+    opponent = "Computer"
+
+os.system('cls')
 
 if (opponent == "Player"):
     game_is_over = False
     #Temporary numbered board
-    tnb = numbered_board
+    lsb = list_number_board
     #Temporary block references
     tbr = block_references
+    #Temporary block list
+    tbs = block_list
     while (game_is_over != True):
-        player_1_turn = input(player_1_turn_message)
-        while bool(player_1_turn in tbr and player_1_turn != "".strip()) == False:
+        game_board = "".join(lsb)
+        print(game_board)
+        player_1_turn = input(player_1_turn_message).strip()
+        while bool(player_1_turn in tbr) == False:
             player_1_turn = input(turn_warning_message)
         tbr = list(map(lambda x: x.replace(player_1_turn, 'O'), tbr))
         tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
@@ -46,15 +51,22 @@ if (opponent == "Player"):
                       [tbr[2], tbr[5], tbr[8]],
                       [tbr[0], tbr[4], tbr[8]],
                       [tbr[2], tbr[4], tbr[6]],]
-        os.system('cls')
-        print(tbr_winning_conditions)
+        for i in range(0,11):
+            for j in range(0, 19):
+                lsb[tbs[int(player_1_turn)-1][i][j]] = o_s[block_x_o[i][j]]
         check_winning_conditions = any(len(set(win_condition)) == 1 for win_condition in tbr_winning_conditions)
         if  check_winning_conditions:
+            os.system('cls')
+            game_board = "".join(lsb)
+            print(game_board)
             print("Player 1 Wins!")
             game_is_over = True
             break
-        player_2_turn = input(player_2_turn_message)
-        while bool(player_2_turn in tbr and player_2_turn != "".strip()) == False:
+        game_board = "".join(lsb)
+        os.system('cls')
+        print(game_board)
+        player_2_turn = input(player_2_turn_message).strip()
+        while bool(player_2_turn in tbr and player_2_turn) == False:
             player_2_turn = input(turn_warning_message)
         tbr = list(map(lambda x: x.replace(player_2_turn, 'X'), tbr))
         tbr_winning_conditions = [[tbr[0], tbr[1], tbr[2]],
@@ -65,10 +77,15 @@ if (opponent == "Player"):
                       [tbr[2], tbr[5], tbr[8]],
                       [tbr[0], tbr[4], tbr[8]],
                       [tbr[2], tbr[4], tbr[6]],]
-        os.system('cls')
-        print(tbr_winning_conditions)
+        for i in range(0,11):
+            for j in range(0, 19):
+                lsb[tbs[int(player_2_turn)-1][i][j]] = x_s[block_x_o[i][j]]
         check_winning_conditions = any(len(set(win_condition)) == 1 for win_condition in tbr_winning_conditions)
         if  check_winning_conditions:
+            os.system('cls')
+            game_board = "".join(lsb)
+            print(game_board)
             print("Player 2 Wins!")
             game_is_over = True
             break
+        os.system('cls')
