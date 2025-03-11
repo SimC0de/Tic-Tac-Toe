692888c5b4012e69ed5c70ad3083747ca32c15e4
SimC0de <yor1simone@outlook.com>
2025-03-11 20:55:32 +0800

    Added more if statements

--- script.py
+++ script.py
@@ -3,6 +3,13 @@ import os
 
 os.system('cls||clear') # Clear terminal everytime py file is executed
 
+#Input messages
+choose_opponent_msg = "\nChoose your opponent!\n1. Computer (PvC)\n2. Player (PvP)\n"
+choose_opponent_x_msg = "\nPlease select No. 1 or No.2:\n1. Computer (PvC)\n2. Player (PvP)\n"
+choose_pvp_msg = "Computer is still in progress, do you want to play pvp?(y/n):\n"
+choose_pvp_x_msg = "Please choose between 'y' (yes) or 'n' (no) to choose PVP.(y/n):\n"
+
+# Tic-Tac-Toe Block
 block = """.--------------..--------------..--------------.
 |  _________   ||     _____    ||     ______   |
 | |  _   _  |  ||    |_   _|   ||   .' ___  |  |
@@ -39,14 +46,10 @@ for i in block:
 time.sleep(1) # Add delay before executing input
 
 # Choose 1 or 2 to pick opponent
-choose_opponent = input("""
-Choose your opponent!
-1. Computer (PvC)
-2. Player (PvP)
-""").strip()
+choose_opponent = input(choose_opponent_msg).strip()
 
 # If 1 or 2 is not chosen, execute this code:
-while choose_opponent != ("1" or "2"):
+while choose_opponent not in ["1", "2"]:
   print("\033[A", end="\r") # Moves the cursor up by one line
   print("\033[K", end="\r") # Clears the whole line
   
@@ -55,10 +58,36 @@ while choose_opponent != ("1" or "2"):
     print("\033[A", end="\r")
   
   # Choose 1 or 2 to pick opponent
-  choose_opponent = input("""
-Please select No. 1 or No.2:
-1. Computer (PvC)
-2. Player (PvP)
-"""
-).strip()
+  choose_opponent = input(choose_opponent_x_msg).strip()
 # End of While Loop
+
+# Computer Opponent
+if choose_opponent == "1":
+  print("\033[A", end="\r") # Moves the cursor up by one line
+  print("\033[K", end="\r") # Clears the whole line
+  
+  # Moves the cursor up and clear the lines by 2
+  for i in range(3):
+    print("\033[A", end="\r")
+    print("\033[K", end="\r")
+    
+  choose_pvp = input(choose_pvp_msg).strip()
+
+  while choose_pvp not in ["y", "n"]:
+    print("\033[A", end="\r") # Moves the cursor up by one line
+    print("\033[K", end="\r") # Clears the whole line
+  
+    # Moves the cursor up by 1 lines
+    for i in range(1):
+      print("\033[A", end="\r")
+    choose_pvp = input(choose_pvp_x_msg).strip()
+  
+  if choose_pvp == 'y':
+    choose_opponent = "2"
+  elif choose_pvp == 'n':
+    os.system('cls||clear')
+    print('buh-bye!')
+
+# Player Opponent
+if choose_opponent == "2":
+  print("No. 2")
